a7ae0c47e9929b117171b785009a5cab6985577f
Drumor <ludovic.taffin@uclouvain.be>
2021-04-07 08:26:35 +0200

    setting celery backend

--- inginious/backend/celery_backend.py
+++ inginious/backend/celery_backend.py
@@ -1,9 +1,6 @@
+import inginious.backend.celeryconfig
 from celery import Celery
 
-app = Celery('tasks', broker='tcp://*:4161')
 
-@app.task
-def add(x, y):
-    return x + y
-
-add(1,2)
+app = Celery('celery_backend')
+app.config_from_object('celeryconfig')
